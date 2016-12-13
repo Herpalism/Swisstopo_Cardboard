c8d7dce511d2ad82c904e734678cde944a1dc4f2
Sebastian <sebastian@ateo.ch>
2016-12-13 11:09:11 +0100

    2nd part of comit before trying to change stuff

---
+++ Assets/Scripts/TextLink.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextLink : MonoBehaviour {
+
+	public string Link=string.Empty;
+
+	void Start(){
+		if (Link==string.Empty) {
+			Text aText= GetComponentInChildren(typeof(Text)) as Text;
+			if (aText!=null) {
+				Link=aText.text;
+			}
+		}
+	}
+
+
+	public void OpenLink()	{
+		Application.OpenURL(Link);
+	}
+}
